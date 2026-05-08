eba7de6a9971b7d17538c57ddae9d24910a35085
Sofiia <sofiiachernezhenko@gmail.com>
2026-05-08 10:37:26 +0200

    added comments to JavaScript and CSS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const InputName = document.getElementById("Name");
 const InputSurname = document.getElementById("Surname");
 const InputDOB = document.getElementById("DOB");
 const InputTlf = document.getElementById("Tlf");
-const InputTerms = document.getElementById("Terms");
+const InputTerms = document.getElementById("Terms"); //creating constants with objects using id from html
 
 function submitForm() {
   if (
@@ -10,16 +10,16 @@ function submitForm() {
     InputSurname.value === "" ||
     InputDOB.value === "" ||
     InputTlf.value === "" ||
-    InputTerms.checked === false
+    InputTerms.checked === false //cheking if all fields were filled
   ) {
-    alert("Vennligst fyll inn alle feltene");
+    alert("Vennligst fyll inn alle feltene"); //pop-up window if more info needed
   } else {
-    alert("Vi har mottatt ditt medlemskap. Velkommen i cluben!");
+    alert("Vi har mottatt ditt medlemskap. Velkommen i cluben!"); //pop-up if everything is filled
     InputName.value = "";
     InputSurname.value = "";
     InputDOB.value = "";
     InputTlf.value = "";
-    InputTerms.checked = false;
+    InputTerms.checked = false; //removing values from all inputs
   }
 }
 
@@ -29,15 +29,15 @@ function submitTur() {
     InputSurname.value === "" ||
     InputDOB.value === "" ||
     InputTlf.value === "" ||
-    InputTerms.checked === false
+    InputTerms.checked === false //cheking if all fields were filled
   ) {
-    alert("Vennligst fyll inn alle feltene");
+    alert("Vennligst fyll inn alle feltene"); //pop-up window if more info needed
   } else {
-    alert("Vi har mottat ditt påmelding. Sees på turen! ");
+    alert("Vi har mottat ditt påmelding. Sees på turen! "); //pop-up if everything is filled
     InputName.value = "";
     InputSurname.value = "";
     InputDOB.value = "";
     InputTlf.value = "";
-    InputTerms.checked = false;
+    InputTerms.checked = false; //removing values from all inputs
   }
 }
